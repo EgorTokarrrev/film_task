bd3ac8ce1954c3d16a6ef0f53d20721caffb61ce
Egor <egortokarev59@gmail.com>
2023-07-11 19:44:37 +0300

    converter2000

---
+++ script2.js
@@ -0,0 +1,10 @@
+// Примитивный конвертер 
+const course = prompt("Введите курс валюты");
+const money = prompt("Введите количество");
+
+function convert(course, money) {
+    alert(course * money);
+    return;
+}
+
+convert(course, money); 
